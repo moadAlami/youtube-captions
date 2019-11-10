47dc63d1970fe23a91943cc264eec80a9dca6bf7
moadAlami <moadalami40@gmail.com>
2019-11-10 15:38:21 +0100

    Edited readme and the py script

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ Looks for a list of words in a YouTube Video
 
 ## Principle
 
-This Python script takes a YouTube video url as input, it then checks if that video has english captions available; if it is the case, it then takes an input list of words from the user and checks if they are in the video. When a match is found, the script will output the timestamp and the sentence containing that word.
+This Python script takes a YouTube video url as input, it then checks if that video has english captions available; if it is the case, it then prompts the user to input a list of words and checks if they are in the video. When a match is found, the script will output the timestamp and the sentence containing that word.
 
-Technically, youtube-captions can also look for sentences, but this can be difficul because most sentences in a video are separated based on the timestamp.
+Technically, youtube-captions can also look for sentences, but this can be difficul because most sentences in a video are split based on the timestamp.
 
 ## False positives
 
-In some videos, if a word that's being searched matches with the person speaking, it can result in a false positive. If we're looking for jimmy for example: "Jimmy: THIS IS ELLEN".
+In some videos, if a word that's being searched matches with the person speaking, it can result in a false positive. For example, If we're looking for the word "Jimmy" --> "Jimmy: THIS IS ELLEN".
 
 ## Dependencies
 
--- YouTube_Captions.py
+++ YouTube_Captions.py
@@ -5,56 +5,60 @@ import re
 import pandas as pd
 import numpy as np
 from pytube import YouTube as YT
+import os
 
-url = input('Please enter the YouTube video link:\n\t $ ')
-while not url == '':
-    print('\nGetting video informations...\n')
-    yt = YT(url)
+try:
+    url = input(':: Please input the YouTube video url:\n > ')
+    while not url == '':
+        print('\nGetting video information...\n')
+        yt = YT(url)
 
-    title = yt.title.replace('/', '-')
-    title = yt.title.replace('\\', '-')
-    title = yt.title.replace('|', '-')
+        title = yt.title.replace('/', '-')
+        title = yt.title.replace('\\', '-')
+        title = yt.title.replace('|', '-')
 
+        print("Getting english captions for '{}'...\n".format(title))
+        captions = yt.captions.get_by_language_code('en')
 
-    print("Getting english captions for '{}'...\n".format(title))
-    captions = yt.captions.get_by_language_code('en')
+        if captions is not None:
+            print('Converting captions to SRT...\n')
+            captions_srt = (captions.generate_srt_captions())
 
-    if captions is not None:
+            with open(r'{}.txt'.format(title), 'w') as file:
+                file.write(re.sub(r'<.+?>', '',captions_srt))
 
-        print('Converting captions to SRT...\n')
-        captions_srt = (captions.generate_srt_captions())
+            word_list = list()
+            with open(r'{}.txt'.format(title), 'r') as file:
+                for line in file:
+                    if not line.isspace():
+                        word_list.append(line) 
 
-        with open(r'{}.txt'.format(title), 'w') as file:
-            file.write(re.sub(r'<.+?>', '',captions_srt))
+            search_list=input(':: Enter the words you want to look for separated by an underscore "_" (no spaces between words):\n > ').split('_')
 
-        word_list = list()
-        with open(r'{}.txt'.format(title), 'r') as file:
-            for line in file:
-                if not line.isspace():
-                    word_list.append(line) 
+            columns = ['Index', 'Time Stamp', 'Sentence']
 
-        search_list=input('Enter the words you want to look for separated by an underscore "_" (no spaces between words):\n\n\t $ ').split('_')
+            df = pd.DataFrame(np.array(word_list).reshape(int(len(word_list)/3),3), columns=columns)
+            df.drop(columns=['Index'], inplace=True)
 
-        columns = ['Index', 'Time Stamp', 'Sentence']
+            for column in columns[1:]:
+                df[column] = df[column].str[:-2]
+                
+            matches = list()
+            for inedx, row in df.iterrows():
+                if re.compile('|'.join(search_list), re.IGNORECASE).search(row[1]):
+                    print('Match found on {}: "{}""'.format(row[0], row[1]))
+                    matches.append(1)   
+            if len(matches) == 0:
+                print('\nNo matches were found for {}'.format(search_list))
 
-        df = pd.DataFrame(np.array(word_list).reshape(int(len(word_list)/3),3), columns=columns)
-        df.drop(columns=['Index'], inplace=True)
-
-        for column in columns[1:]:
-            df[column] = df[column].str[:-2]
+            os.remove('{}.txt'.format(title))
             
-        matches = list()
-        for inedx, row in df.iterrows():
-            if re.compile('|'.join(search_list), re.IGNORECASE).search(row[1]):
-                print('Match found on {}: "{}""'.format(row[0], row[1]))
-                matches.append(1)   
-        if len(matches) == 0:
-            print('\nNo matches were found for {}'.format(search_list))
-        
-        input('\nClick enter to move to search a new video')
+            input('\nClick "ENTER" to search in another video')
 
-    else:
-        print('Captions are not available for this video!')
-        input('\nClick enter to move to search a new video')
+        else:
+            print('Captions are not available for this video!')
+            input('\nClick "ENTER" to search in another video')
 
-    url = input('\nPlease enter the YouTube video link (Press \'Enter\' to quit):\n\t $ ')
+        url = input('\n:: Please enter the YouTube video link (Press \'Enter\' to quit):\n > ')
+except Exception as e:
+    print(str(e))
